11fb87ce01a3026ff9d0b5ae8648aba7dc3c1f6d
hnhaefliger <henry_haefliger@protonmail.com>
2021-11-10 17:07:38 -0500

    store data about visited tabs and make all data persistent across sessions

--- src/js/background.js
+++ src/js/background.js
@@ -1,6 +1,63 @@
-chrome.runtime.onInstalled.addListener(() => {
+chrome.runtime.onInstalled.addListener((e) => {
+  resetSession();
+});
+
+chrome.runtime.onMessage.addListener((e) => {
+  handleMessage(e);
+});
+
+chrome.tabs.onActivated.addListener((e) => {
+  saveTabData(e.tabId);
+});
+
+chrome.tabs.onUpdated.addListener((e) => {
+  saveTabData(e);
+});
+
+function updatePopup(scope='display') {
+  chrome.runtime.sendMessage('update ' + scope);
+}
+
+function resetSession() {
   chrome.storage.local.set({sessionName: ''});
   chrome.storage.local.set({sessionDescription: '',});
   chrome.storage.local.set({sessionData: 'Recorded data:',});
   chrome.storage.local.set({sessionState: 'idle'});
-});
+}
+
+function saveTabData(tabId) {
+  chrome.tabs.get(tabId, (e) => {
+    if (e.url) {
+      chrome.storage.local.get(['sessionState', 'sessionData'], (data) => {
+        if (data.sessionState === 'running' && e.url !== data.sessionData.split('\n').pop()) {
+          chrome.storage.local.set({sessionData: data.sessionData + '\n' + e.url}, () => {
+            updatePopup(scope='data')
+          });
+        }
+      });
+    }
+  });
+}
+
+function handleMessage(message) {
+  if (message === 'start') {
+    chrome.storage.local.set({sessionState: 'running'}, () => {
+      updatePopup();
+    });
+  } else {
+    if (message === 'end') {
+      chrome.storage.local.set({sessionState: 'pending'}, () => {
+        updatePopup();
+      });
+    } else {
+      if (message === 'reset') {
+        // send data to server
+
+        chrome.storage.local.set({sessionState: 'idle'}, () => {
+          resetSession();
+          updatePopup();
+        });
+      }
+    }
+  }
+}
--- src/js/popup.js
+++ src/js/popup.js
@@ -2,63 +2,101 @@ sessionIcon = document.getElementById('session-icon');
 sessionName = document.getElementById('session-name');
 sessionDescription = document.getElementById('session-description');
 sessionData = document.getElementById('session-data');
+dataSection = document.getElementById('data-section');
 sessionStart = document.getElementById('session-start');
+sessionCancel = document.getElementById('session-cancel');
+cancelSection = document.getElementById('cancel-section');
 sessionState = 'idle';
 
-chrome.storage.local.get(['sessionState', 'sessionName', 'sessionDescription', 'sessionData'], function(result) {
-  sessionName.value = result.sessionName;
-  sessionDescription.value = result.sessionDescription;
-  sessionData.innerHTML = result.sessionData;
-  sessionState = result.sessionState
+sessionName.addEventListener('input', (e) => {
+  chrome.storage.local.set({'sessionName': sessionName.value});
+});
 
-  if (sessionState === 'idle') {
-    sessionIcon.classList.add('ion-ios-barcode-outline');
-    sessionStart.innerHTML = 'Start session';
-
-  } else {
-    if (sessionState === 'running') {
-      sessionIcon.classList.add('ion-ios-eye-outline');
-      sessionStart.innerHTML = 'End session';
-
-    } else {
-      if (sessionState === 'pending') {
-        sessionIcon.classList.add('ion-ios-cloud-upload-outline');
-        sessionStart.innerHTML = 'Confirm submit';
-
-      }
-    }
-  }
+sessionDescription.addEventListener('input', (e) => {
+  chrome.storage.local.set({'sessionDescription': sessionDescription.value});
 });
 
 sessionStart.addEventListener('click', (e) => {
   e.preventDefault();
 
   if (sessionState === 'idle') {
-    chrome.storage.local.set({sessionState: 'running'}, () => {
-      sessionState = 'running';
-      sessionIcon.classList.remove('ion-ios-barcode-outline');
-      sessionIcon.classList.add('ion-ios-eye-outline');
-      sessionStart.innerHTML = 'End session';
-    });
+    chrome.runtime.sendMessage('start');
 
   } else {
     if (sessionState === 'running') {
-      chrome.storage.local.set({sessionState: 'pending'}, () => {
-        sessionState = 'pending';
-        sessionIcon.classList.remove('ion-ios-eye-outline');
-        sessionIcon.classList.add('ion-ios-cloud-upload-outline');
-        sessionStart.innerHTML = 'Confirm submit';
-      });
+      chrome.runtime.sendMessage('end');
 
     } else {
       if (sessionState === 'pending') {
-        chrome.storage.local.set({sessionState: 'idle'}, () => {
-          sessionState = 'idle';
-          sessionIcon.classList.remove('ion-ios-cloud-upload-outline');
-          sessionIcon.classList.add('ion-ios-barcode-outline');
-          sessionStart.innerHTML = 'Start session';
-        });
+        chrome.runtime.sendMessage('reset');
       }
     }
   }
 });
+
+sessionCancel.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  if (sessionState === 'pending') {
+    chrome.runtime.sendMessage('reset');
+  }
+});
+
+chrome.runtime.onMessage.addListener((e) => {
+  handleMessage(e);
+});
+
+function handleMessage(message) {
+  message = message.split(' ');
+
+  if (message[0] === 'update') {
+    if (message[1] === 'display') {
+      updateDisplay();
+    } else {
+      if (message[1] === 'data') {
+        updateData();
+      }
+    }
+  }
+}
+
+function updateDisplay() {
+  chrome.storage.local.get(['sessionState', 'sessionName', 'sessionDescription', 'sessionData'], function(result) {
+    sessionName.value = result.sessionName;
+    sessionDescription.value = result.sessionDescription;
+    sessionData.innerHTML = result.sessionData;
+    sessionData.scrollTop = sessionData.scrollHeight;
+    sessionState = result.sessionState
+
+    if (sessionState === 'idle') {
+      sessionIcon.classList.add('ion-ios-barcode-outline');
+      sessionStart.innerHTML = 'Start session';
+      cancelSection.classList.add('hidden');
+      dataSection.classList.add('hidden');
+
+    } else {
+      if (sessionState === 'running') {
+        sessionIcon.classList.add('ion-ios-eye-outline');
+        sessionStart.innerHTML = 'End session';
+        cancelSection.classList.add('hidden');
+        dataSection.classList.remove('hidden');
+
+      } else {
+        if (sessionState === 'pending') {
+          sessionIcon.classList.add('ion-ios-cloud-upload-outline');
+          sessionStart.innerHTML = 'Confirm submission';
+          cancelSection.classList.remove('hidden');
+          dataSection.classList.remove('hidden');
+        }
+      }
+    }
+  });
+}
+
+function updateData() {
+  chrome.storage.local.get(['sessionData'], function(result) {
+    sessionData.innerHTML = result.sessionData;
+  });
+}
+
+updateDisplay();
